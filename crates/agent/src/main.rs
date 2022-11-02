09c6d1981727e6d97787607259edb2a7c661999a
Alexander Sieg <alex@xanderio.de>
2022-11-02 20:24:02 +0100

    agent: backoff and reconnect on connection failure

--- crates/agent/src/main.rs
+++ crates/agent/src/main.rs
@@ -1,10 +1,11 @@
-use std::{env::args, path::PathBuf, sync::Mutex};
+use std::{env::args, path::PathBuf, sync::Mutex, time::Duration};
 
 use color_eyre::Result;
 use futures_util::{SinkExt, TryStreamExt};
 use once_cell::sync::Lazy;
 use state::State;
-use tokio_tungstenite::{connect_async, tungstenite::Message};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
 use tracing::instrument;
 
 use rpc::{JsonRPC, Request, Response};
@@ -30,20 +31,50 @@ async fn main() -> Result<()> {
 }
 
 async fn run() -> Result<()> {
-    let (mut ws, _) = connect_async("ws://localhost:8080/ws").await?;
-
-    while let Some(msg) = ws.try_next().await? {
-        let rpc: JsonRPC = msg.into_text()?.parse()?;
-        match rpc {
-            JsonRPC::Request(request) => {
-                let res: JsonRPC = handle_request(request).await?.into();
-                ws.send(Message::Text(res.to_string())).await?;
+    loop {
+        let (mut ws, _) = connect().await?;
+        while let Ok(Some(msg)) = ws.try_next().await {
+            let rpc: JsonRPC = msg.into_text()?.parse()?;
+            match rpc {
+                JsonRPC::Request(request) => {
+                    let res: JsonRPC = handle_request(request).await?.into();
+                    ws.send(Message::Text(res.to_string())).await?;
+                }
+                JsonRPC::Response(res) => {
+                    tracing::warn!(?res, "received response, this should happen")
+                }
+                JsonRPC::Notification(notification) => tracing::info!(?notification),
             }
-            JsonRPC::Response(res) => tracing::warn!(?res, "received response, this should happen"),
-            JsonRPC::Notification(notification) => tracing::info!(?notification),
         }
     }
-    Ok(())
+}
+
+async fn connect() -> Result<(
+    WebSocketStream<MaybeTlsStream<TcpStream>>,
+    tokio_tungstenite::tungstenite::http::Response<()>,
+)> {
+    let mut retry_period = Duration::from_millis(500);
+    loop {
+        match connect_async("ws://localhost:8080/ws").await {
+            Ok(ws) => return Ok(ws),
+            Err(e) => {
+                tracing::warn!(
+                    "unable to astablish connection to server, retrying in {:?}",
+                    retry_period
+                );
+                tracing::debug!(?e);
+                tokio::time::sleep(retry_period).await;
+                retry_period = backoff(retry_period);
+            }
+        }
+    }
+}
+
+fn backoff(duration: Duration) -> Duration {
+    if duration >= Duration::from_secs(4) {
+        return Duration::from_secs(4);
+    }
+    duration * 2
 }
 
 #[instrument(skip_all, err, fields(id = %request.id, method = request.method))]
