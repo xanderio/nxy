b8655a31d823fa13b1b4eeba6e41b26b0b7da52c
Alexander Sieg <alex@xanderio.de>
2022-11-29 21:02:15 +0100

    server: api: expose list of all agents

--- nxy-server/src/http/agent/mod.rs
+++ nxy-server/src/http/agent/mod.rs
@@ -1,9 +1,33 @@
 mod websocket;
 
-use axum::{routing::get, Router};
+use axum::{extract::State, routing::get, Json, Router};
+use serde::Serialize;
+use uuid::Uuid;
 
-use super::ApiContext;
+use super::{ApiContext, Result};
 
 pub(crate) fn router() -> Router<ApiContext> {
-    Router::new().route("/api/v1/agent/ws", get(websocket::ws_handler))
+    Router::new()
+        .route("/api/v1/agent", get(get_agents))
+        .route("/api/v1/agent/ws", get(websocket::ws_handler))
+}
+
+#[derive(Serialize)]
+struct Agent {
+    id: Uuid,
+    current_system: Option<String>,
+}
+
+async fn get_agents(ctx: State<ApiContext>) -> Result<Json<Vec<Agent>>> {
+    let agents = sqlx::query!("SELECT agent_id, current_system FROM agents")
+        .fetch_all(&ctx.db)
+        .await?
+        .into_iter()
+        .map(|row| Agent {
+            id: row.agent_id,
+            current_system: row.current_system,
+        })
+        .collect();
+
+    Ok(Json(agents))
 }
