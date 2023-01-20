364e630c298d33c504816def2934c0bb8e74a39b
Alexander Sieg <alex@xanderio.de>
2023-01-20 14:20:19 +0100

    cli: rename binary to nxy

--- checks/clients-connect/test-script.py
+++ checks/clients-connect/test-script.py
@@ -6,11 +6,11 @@ agent_id = alpha.succeed("jq '.id' /var/lib/nxy/state.json").strip()
 
 with subtest("copy store path to agent"):
     alpha.fail(f"nix path-info {test_store_path}")
-    server.succeed(f"NXY_SERVER=http://server nxy-cli agents download {agent_id} {test_store_path}")
+    server.succeed(f"NXY_SERVER=http://server nxy agents download {agent_id} {test_store_path}")
     alpha.succeed(f"nix path-info {test_store_path}")
 
 with subtest("activate configuration"):
-    server.succeed(f"NXY_SERVER=http://server nxy-cli agents activate {agent_id} {test_store_path}")
+    server.succeed(f"NXY_SERVER=http://server nxy agents activate {agent_id} {test_store_path}")
 
 with subtest("profile switched to new path"):
     profile_path = alpha.succeed("readlink -f /nix/var/nix/profiles/system").strip()
